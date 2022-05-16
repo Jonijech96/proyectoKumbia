06674b6495c95efeaf8a46ef88d5f9874f0603b9
Jonathan <joni13_escorpio@gmail.com>
2022-05-16 06:23:53 -0300

    agregando efectos de scroll1

--- js/scrollHeader.js
+++ js/scrollHeader.js
@@ -2,3 +2,16 @@ window.addEventListener("scroll", function () {
   var header = document.querySelector("header");
   header.classList.toggle("abajo", window.scrollY > 0);
 });
+
+sr = ScrollReveal();
+
+sr.reveal(".quienes-somos-texto", {
+  duration: 3000,
+  origin: "right",
+  distance: "-100px",
+});
+sr.reveal(".quienes-somos-imagen", {
+  duration: 3000,
+  origin: "left",
+  distance: "-100px",
+});
