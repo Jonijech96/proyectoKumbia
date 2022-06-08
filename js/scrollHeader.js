2bc9b3ee62461c5c7be9122d8698832d10fb23e0
Jonathan <joni13_escorpio@gmail.com>
2022-06-08 06:11:59 -0300

    agregando ultimos detalles de diseño de pagina

--- js/scrollHeader.js
+++ js/scrollHeader.js
@@ -31,3 +31,40 @@ sr.reveal(".quienes-somos-imagen", {
   origin: "left",
   distance: "-100px",
 });
+
+window.addEventListener("load", function () {
+  new Glider(document.querySelector(".carrousel-imagenes"), {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    draggable: true,
+    rewind: true,
+    dots: ".galeria-indicadores",
+    arrows: {
+      prev: ".galeria-anterior",
+      next: ".galeria-siguiente",
+    },
+    responsive: [
+      {
+        // screens greater than >= 775px
+        breakpoint: 775,
+        settings: {
+          // Set to `auto` and provide item width to adjust to viewport
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          itemWidth: 150,
+          duration: 0.25,
+        },
+      },
+      {
+        // screens greater than >= 1024px
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          itemWidth: 150,
+          duration: 0.25,
+        },
+      },
+    ],
+  });
+});
